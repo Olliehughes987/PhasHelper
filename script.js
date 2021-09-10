f3dc4790851f739acc3df499f608578f9fa7a56b
olliehughes987 <p2421386@my365.dmu.ac.uk>
2021-09-10 20:25:24 +0100

    wrote list of ghost objects

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function updateEvidence() {
-    console.log("test");
-}
+const Ghosts = [
+    {name:"Banshee",evidence:["Dots","Orb","Fingerprints"]},
+    {name:"Demon",evidence:["Writing","Fingerprints","Freezing"]},
+    {name:"Goryo",evidence:["Dots","EMF","Fingerprints"]},
+    {name:"Hantu",evidence:["Orb","Fingerprints","Freezing"]},
+    {name:"Jinn",evidence:["EMF","Fingerprints","Freezing"]},
+    {name:"Mare",evidence:["Writing","Orb","SpiritBox"]},
+    {name:"Myling",evidence:["Writing","EMF","Fingerprints"]},
+    {name:"Oni",evidence:["Dots","EMF","Freezing"]},
+    {name:"Phantom",evidence:["Dots","Fingerprints","SpiritBox"]},
+    {name:"Poltergeist",evidence:["Writing","Fingerprints","SpiritBox"]},
+    {name:"Revenant",evidence:["Writing","Orb","Freezing"]},
+    {name:"Shade",evidence:["Writing","EMF","Freezing"]},
+    {name:"Spirit",evidence:["Writing","EMF","SpiritBox"]},
+    {name:"Wraith",evidence:["Dots","EMF","SpiritBox"]},
+    {name:"Yokai",evidence:["Dots","Orb","SpiritBox"]},
+    {name:"Yurei",evidence:["Dots","Orb","Freezing"]}
+
+];
+
+
+window.addEventListener('load', () => {
+    console.log(Ghosts);
+})
